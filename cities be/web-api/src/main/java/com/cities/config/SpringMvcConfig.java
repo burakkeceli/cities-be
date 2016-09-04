409ef5ca4b4bb6ac58218da32bea703205cf2377
burak <kecelibu@gmail.com>
2016-09-04 15:34:32 +0200

    created project structure and added an example of restful service

---
+++ cities be/web-api/src/main/java/com/cities/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package com.cities.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.cities")
+public class SpringMvcConfig {
+}
