6b8cc06773d41805211f4b3b87d5131f10589f27
burak <kecelibu@gmail.com>
2016-09-04 15:46:53 +0200

    correction of project structure

---
+++ web-api/src/main/java/com/cities/city/CityController.java
@@ -0,0 +1,21 @@
+package com.cities.city;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class CityController {
+
+    @RequestMapping(value = "/city/", method = GET)
+    public ResponseEntity listCities() {
+        List cities = Arrays.asList("Berlin", "Istanbul");
+        return new ResponseEntity<>(cities, OK);
+    }
+}
