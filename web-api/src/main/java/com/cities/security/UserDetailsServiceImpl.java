0478c0fc398d9599836fe10e0333ce381d45c76a
burak <kecelibu@gmail.com>
2016-09-26 20:27:07 +0200

    refactoring

--- web-api/src/main/java/com/cities/security/UserDetailsServiceImpl.java
+++ web-api/src/main/java/com/cities/security/UserDetailsServiceImpl.java
@@ -8,15 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import static org.apache.commons.lang.StringUtils.equalsIgnoreCase;
 import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @Service
