23c861b8ad7b579d560c2baf4fd4bfaedd4f43ca
burak <kecelibu@gmail.com>
2016-10-01 16:31:28 +0200

    code refactorin - code correction

--- web-api/src/main/java/com/cities/security/UserDetailsServiceImpl.java
+++ web-api/src/main/java/com/cities/security/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cities.security;
 
 import com.cities.model.user.User;
+import com.cities.model.user.UserRole;
 import com.cities.service.UserService;
 import com.cities.user.UserDto;
 import org.apache.log4j.Logger;
@@ -10,6 +11,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @Service
@@ -28,9 +36,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
 
         return new UserDto(user.getId(),
-                                      user.getName(),
-                                      user.getPassword(),
-                                      null,
-                                      createAuthorityList("ROLE_ADMIN", "ROLE_USER"));
+                           user.getName(),
+                           user.getPassword(),
+                           null,
+                           createAuthorityList(getRoles(user)));
+    }
+
+    private String[] getRoles(User user) {
+        List<String> rolesList = user.getUserRoles().stream().map(UserRole::getRole).collect(toList());
+        return rolesList.toArray(new String[rolesList.size()]);
     }
 }
