489268adb56508f86e7be9ff4435377fdf72a246
burak <kecelibu@gmail.com>
2017-01-15 18:25:48 +0100

    swagger integration has been done

--- web-api/src/main/java/com/cities/config/RootConfig.java
+++ web-api/src/main/java/com/cities/config/RootConfig.java
@@ -1,6 +1,7 @@
 package com.cities.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan(basePackages = {"com.*"},
                 excludeFilters = {
-                    @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+                    @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
                 })
 @Import(PersistenceConfig.class)
 public class RootConfig {
--- web-api/src/main/java/com/cities/config/SpringMvcConfig.java
+++ web-api/src/main/java/com/cities/config/SpringMvcConfig.java
@@ -1,11 +1,34 @@
 package com.cities.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.any;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @EnableWebMvc
 @ComponentScan(basePackages = "com.*")
-public class SpringMvcConfig {
+@EnableSwagger2
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public Docket api() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
