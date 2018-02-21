583ed9068a322b92d6849b30b769dc4011f6ffc9
burak <kecelibu@gmail.com>
2018-02-22 00:10:20 +0100

    Enable Jenkins in docker to connect git, download war and deploy it to docker tomcat

--- scripts/startDockerEnvironment.sh
+++ scripts/startDockerEnvironment.sh
@@ -1,6 +1,5 @@
 {
     docker stop $(docker ps -a -q) &&
-    docker rm $(docker ps -a -q) &&
     docker-compose -f docker-compose.yaml up -d
 } || {
     docker-compose -f docker-compose.yaml up -d
--- scripts/startLocalEnvironment.sh
+++ scripts/startLocalEnvironment.sh
@@ -1,6 +1,5 @@
 {
     docker stop $(docker ps -a -q) &&
-    docker rm $(docker ps -a -q) &&
     docker-compose -f docker-compose-local.yaml up -d
 } || {
     docker-compose -f docker-compose-local.yaml up -d
